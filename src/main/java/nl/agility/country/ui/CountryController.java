43bc93b6b7c425165093d81271702dec0101d1ff
aduursma <andre@agility-it-services.nl>
2020-03-27 17:19:42 +0100

    Changed RequestMapping into GetMapping

--- src/main/java/nl/agility/country/ui/CountryController.java
+++ src/main/java/nl/agility/country/ui/CountryController.java
@@ -3,6 +3,7 @@ package nl.agility.country.ui;
 import lombok.RequiredArgsConstructor;
 import nl.agility.country.domain.Country;
 import nl.agility.country.service.CountryService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class CountryController {
 
     private final CountryService countryService;
 
-    @RequestMapping("/countries")
+    @GetMapping("/countries")
     public List<Country> retrieveCountries() {
         return this.countryService.retrieveCountries();
     }
